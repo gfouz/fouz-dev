00d676ad00822e767ff118e21d0cecb757cebc57
gfouz <gfouz1975@gmail.com>
2023-12-13 04:21:48 -0500

    new folder added

---
+++ docs/_next/static/chunks/app/error-37ef3d681c6e44c3.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[601],{2807:function(r,e,n){Promise.resolve().then(n.bind(n,988))},988:function(r,e,n){"use strict";n.r(e),n.d(e,{default:function(){return Error}});var t=n(7437),o=n(2265);function Error(r){let{error:e,reset:n}=r;return(0,o.useEffect)(()=>{console.error(e)},[e]),(0,t.jsxs)("div",{children:[(0,t.jsx)("h1",{children:"Something wrong ocurred!"}),(0,t.jsx)("button",{onClick:()=>n(),children:"Try again"})]})}},622:function(r,e,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),u=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),s=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(r,e,n){var t,f={},i=null,l=null;for(t in void 0!==n&&(i=""+n),void 0!==e.key&&(i=""+e.key),void 0!==e.ref&&(l=e.ref),e)u.call(e,t)&&!c.hasOwnProperty(t)&&(f[t]=e[t]);if(r&&r.defaultProps)for(t in e=r.defaultProps)void 0===f[t]&&(f[t]=e[t]);return{$$typeof:o,type:r,key:i,ref:l,props:f,_owner:s.current}}e.jsx=q,e.jsxs=q},7437:function(r,e,n){"use strict";r.exports=n(622)}},function(r){r.O(0,[971,472,744],function(){return r(r.s=2807)}),_N_E=r.O()}]);
